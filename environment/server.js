9660e7564924e9e76698a967f4cf612956a16c1c
emon199227 <emon199227@gmail.com>
2022-09-13 01:51:52 +0600

    node js environment

---
+++ environment/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT =process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send(`Hello I am home route`);
+});
+
+app.listen(PORT,()=>{
+    console.log(`server at running  at http://localhost:${PORT}`);
+
+});
