1f0637ae4a686567c212cd3914acb99be5cf0306
emon199227 <emon199227@gmail.com>
2022-09-27 15:33:15 +0600

    website authontication encryption password

---
+++ encrypt_database/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const userSchema =new mongoose.Schema({
+    email:{
+        type:String,
+        require:true,
+    },
+    password:{
+        type:String,
+        require:true,
+    },
+    createdOn:{
+        type:Date,
+        default:Date.now,
+    },
+});
+
+const enckey=process.env.ENC_KEY;
+userSchema.plugin(encrypt,{secret:enckey,encryptedFields:["password"],});
+module.exports = mongoose.model("user",userSchema);
